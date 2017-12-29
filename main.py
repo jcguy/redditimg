9357de416a15a252d3efb4b554f3ccf64c62ed4f
James Corder Guy <corder.guy@gmail.com>
2017-12-28 20:32:08 -0600

    Initial commit

---
+++ main.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# Downloads images from a given subreddit
+
+
+import json
+import praw
+
+
+def main():
+    reddit = praw.Reddit(client_id=)
+    pass
+
+
+if __name__ == "__main__":
+    main()
